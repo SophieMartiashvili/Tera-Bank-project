f9d351af4682cbca4334494c126c6b4a30d737a6
localadmin <localadmin@DESKTOP-TNS2OCA>
2023-03-11 22:10:14 +0400

    Add project files.

---
+++ Tera Bank project/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+class MyMain
+{
+    static void Main(string[] args) // ყველა პროგრამის დასაწყებად საჭიროა, entry point
+    {
+        //გამომყავს კონსოლში შეტყობინება single line comment
+       Console.WriteLine("Hello Sophie"); // writeline-ს გამოყავს ტექსტი + გადადის ახალ სტრიქონზე
+       Console.WriteLine("Mr. Archil");
+
+       Console.WriteLine("this is a second message");
+       Console.WriteLine("myself...");
+    }
+
+
+
+
+
+}
+//
+/*rghsh
+ * 
+ */
+
+///this method
+
