93de9f3e60df8b9938ed9f269439d6077e5f9466
Sopiko Martiashvili <sopiko.martiashvili@tera.ge>
2023-03-23 21:49:54 +0400

    update

--- Tera Bank project/Program.cs
+++ Tera Bank project/Program.cs
@@ -2,33 +2,111 @@
 //Console.WriteLine("Hello, World!");
 class MyMain
 {
-    static void Main(string[] args) // ყველა პროგრამის დასაწყებად საჭიროა, entry point
+    static void Main(string[] args)
     {
-        //გამომყავს კონსოლში შეტყობინება single line comment
-       Console.WriteLine("Hello Sophie"); // writeline-ს გამოყავს ტექსტი + გადადის ახალ სტრიქონზე
-       Console.WriteLine("Mr. Archil");
+        // 1)დაწერეთ პროგრამა რომელიც გამოთვლის კონსოლიდან შეყვანილი  ორი რიცხვის ნამრავლს.
 
-       Console.WriteLine("this is a second message");
-       Console.WriteLine("myself...");
-        Console.WriteLine("Hello Sophie");
-        guygyug
+        Console.WriteLine("Enter first number:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter second number:");
+        int b = Convert.ToInt32(Console.ReadLine());
+        int result = a * b;
+        Console.WriteLine("Multiplication of given two numbers is: " + result);
+
+        // 2)დაწერეთ პროგრამა, რომელიც კონსოლზე გამოიტანს თქვენ სახელს, გვარს და ასაკს.
+        string firstName = "Sophie";
+        string lastName = "Martiashvili";
+        string age = "33";
+        string fullNameage = firstName + lastName + age;
+        Console.WriteLine(fullNameage);
+
+        // 3)დაწერეთ პროგრამა რომელიც გამოიტანს ნაშთს, კონსოლიდან გადაცემული ორი რიცხვის გაყოფის შედეგად
+
+        Console.WriteLine("Enter first number:");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter second number:");
+        int y = Convert.ToInt32(Console.ReadLine());
+        int result1 = x / y;
+        Console.WriteLine("The result of dividing the entered numbers is: " + result1);
+
+
+        // 4)შედარების ლოგიკური ოპერატორების საშუალებით შეადარეთ რიცხვები და დაწერეთ პროგრამა, რომელიც გამოიტანს true ან false;
+        // 4. 1 
+        int number1 = 8;
+        int number2 = 7;
+        Console.WriteLine(number1 > number2);
+        //4.2
+        int a1 = 10;
+        int a2 = 8;
+        Console.WriteLine(a1 <= a2);
+
+        // 5)ჩაწერეთ შემდეგი პირობა Boolean ლოგიკური ოპერატორის გამოყენებით: A>B  და A>C, A>B ან A>C. როცა A=5,B=4,C=7
+        int A = 5;
+        int B = 4;
+        int C = 7;
+
+        Console.WriteLine(A > B && A > C);
+        Console.WriteLine(A > B || A > C);
+
+        // 6)შექმენით რიცხვითი ტიპის, 10 ელემენტისაგან შემდგარი მასივი. კონსოლზე გამოიტანეთ პირველი და მეშვიდე ელემენტი.
+        int[] numbers = new int[10];
+        numbers[0] = 10;
+        numbers[1] = 11;
+        numbers[2] = 12;
+        numbers[3] = 13;
+        numbers[4] = 14;
+        numbers[5] = 15;
+        numbers[6] = 16;
+        numbers[7] = 17;
+        numbers[8] = 18;
+        numbers[9] = 19;
+
+        Console.WriteLine(numbers[0]);
+        Console.WriteLine(numbers[6]);
+
+        // 7)7) შექმენით მასივები შემდეგი განზომილებებით და შეავსე რიცხვებით:
+        //ორგანზომილებიანი[3, 2], [4,2]
+
+        int[,] array2 = new int[3, 2]
+       { { 5, 6 },
+         { 7, 8 },
+         { 9, 10 } };
+
+        int[,] array4 = new int[4, 2]
+       { { 5, 6 },
+         { 7, 8 },
+         { 9, 10 },
+         {11,12 } };
+
+        //სამგანზომილებიანი[2, 2, 2], [1,2,3]
+
+        int[,,] arr3 = new int[2, 2, 2]
+       { 
+         {{11,24 },{18,48}},
+         {{ 62,72}, { 91,10}}
+       };
+
+
+
+        int[,,] arr4 = new int[1, 2, 3]
+           {{{45,80,87}, {52,85,98}}};
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
-
-    //გიტს აქვს რამოდენიმე ტერმინი
-    //stage
-    //Commit
-    //Push
-
-    //Clone -დავაკოპიროთ მთლიანად რეპოზიტორი/Clone the repository
-
-    //Basic Code > improvements> Stage (prepared for Git) > Commit "message" (Fully implement to Git) > Push (send to Github) >
-
 }
-//
-/*rghsh
- * 
- */
 
-///this method
+
+
+
 
