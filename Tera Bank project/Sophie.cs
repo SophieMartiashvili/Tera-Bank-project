c6988a91c810be59af232a215bd78dbd79fe086b
Sopiko Martiashvili <sopiko.martiashvili@tera.ge>
2023-03-29 22:32:49 +0400

    ffse

---
+++ Tera Bank project/Sophie.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        MyFunction my = Sum;
+        int result = my(5, 4);
+        Console.WriteLine(result);
+
+    }
+    delegate int MyFunction(int a, int b);
+    public static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+
+
+
+
+
+
+
+}
