a2cab3bb1329ecc848338902c1fcae87e81b1797
Sopiko Martiashvili <sopiko.martiashvili@tera.ge>
2023-04-02 22:04:55 +0400

    dhdrrd

--- Tera Bank project/Program.cs
+++ Tera Bank project/Program.cs
@@ -1,97 +1,95 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-class MyMain
-{
-    static void Main(string[] args)
-    {
-        // 1)დაწერეთ პროგრამა რომელიც გამოთვლის კონსოლიდან შეყვანილი  ორი რიცხვის ნამრავლს.
+﻿//// See https://aka.ms/new-console-template for more information
+////Console.WriteLine("Hello, World!");
+//class MyMain
+//{
+//    static void Main(string[] args)
+//    {
+//        // 1)დაწერეთ პროგრამა რომელიც გამოთვლის კონსოლიდან შეყვანილი  ორი რიცხვის ნამრავლს.
 
-        Console.WriteLine("Enter first number:");
-        int a = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter second number:");
-        int b = Convert.ToInt32(Console.ReadLine());
-        int result = a * b;
-        Console.WriteLine("Multiplication of given two numbers is: " + result);
+//Console.WriteLine("Enter first number:");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Enter second number:");
+//int b = Convert.ToInt32(Console.ReadLine());
+//int result = a * b;
+//Console.WriteLine("Multiplication of given two numbers is: " + result);
 
-        // 2)დაწერეთ პროგრამა, რომელიც კონსოლზე გამოიტანს თქვენ სახელს, გვარს და ასაკს.
-        string firstName = "Sophie";
-        string lastName = "Martiashvili";
-        string age = "33";
-        string fullNameage = firstName + lastName + age;
-        Console.WriteLine(fullNameage);
+//        // 2)დაწერეთ პროგრამა, რომელიც კონსოლზე გამოიტანს თქვენ სახელს, გვარს და ასაკს.
+//        string firstName = "Sophie";
+//        string lastName = "Martiashvili";
+//        string age = "33";
+//        string fullNameage = firstName + lastName + age;
+//        Console.WriteLine(fullNameage);
 
-        // 3)დაწერეთ პროგრამა რომელიც გამოიტანს ნაშთს, კონსოლიდან გადაცემული ორი რიცხვის გაყოფის შედეგად
+//        // 3)დაწერეთ პროგრამა რომელიც გამოიტანს ნაშთს, კონსოლიდან გადაცემული ორი რიცხვის გაყოფის შედეგად
 
-        Console.WriteLine("Enter first number:");
-        int x = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter second number:");
-        int y = Convert.ToInt32(Console.ReadLine());
-        int result1 = x / y;
-        Console.WriteLine("The result of dividing the entered numbers is: " + result1);
-
-
-        // 4)შედარების ლოგიკური ოპერატორების საშუალებით შეადარეთ რიცხვები და დაწერეთ პროგრამა, რომელიც გამოიტანს true ან false;
-        // 4. 1 
-        int number1 = 8;
-        int number2 = 7;
-        Console.WriteLine(number1 > number2);
-        //4.2
-        int a1 = 10;
-        int a2 = 8;
-        Console.WriteLine(a1 <= a2);
-
-        // 5)ჩაწერეთ შემდეგი პირობა Boolean ლოგიკური ოპერატორის გამოყენებით: A>B  და A>C, A>B ან A>C. როცა A=5,B=4,C=7
-        int A = 5;
-        int B = 4;
-        int C = 7;
-
-        Console.WriteLine(A > B && A > C);
-        Console.WriteLine(A > B || A > C);
-
-        // 6)შექმენით რიცხვითი ტიპის, 10 ელემენტისაგან შემდგარი მასივი. კონსოლზე გამოიტანეთ პირველი და მეშვიდე ელემენტი.
-        int[] numbers = new int[10];
-        numbers[0] = 10;
-        numbers[1] = 11;
-        numbers[2] = 12;
-        numbers[3] = 13;
-        numbers[4] = 14;
-        numbers[5] = 15;
-        numbers[6] = 16;
-        numbers[7] = 17;
-        numbers[8] = 18;
-        numbers[9] = 19;
-
-        Console.WriteLine(numbers[0]);
-        Console.WriteLine(numbers[6]);
-
-        // 7)7) შექმენით მასივები შემდეგი განზომილებებით და შეავსე რიცხვებით:
-        //ორგანზომილებიანი[3, 2], [4,2]
-
-        int[,] array2 = new int[3, 2]
-       { { 5, 6 },
-         { 7, 8 },
-         { 9, 10 } };
-
-        int[,] array4 = new int[4, 2]
-       { { 5, 6 },
-         { 7, 8 },
-         { 9, 10 },
-         {11,12 } };
-
-        //სამგანზომილებიანი[2, 2, 2], [1,2,3]
-
-        int[,,] arr3 = new int[2, 2, 2]
-       { 
-         {{11,24 },{18,48}},
-         {{ 62,72}, { 91,10}}
-       };
+//        Console.WriteLine("Enter first number:");
+//        int x = Convert.ToInt32(Console.ReadLine());
+//        Console.WriteLine("Enter second number:");
+//        int y = Convert.ToInt32(Console.ReadLine());
+//        int result1 = x / y;
+//        Console.WriteLine("The result of dividing the entered numbers is: " + result1);
 
 
+//        // 4)შედარების ლოგიკური ოპერატორების საშუალებით შეადარეთ რიცხვები და დაწერეთ პროგრამა, რომელიც გამოიტანს true ან false;
+//        // 4. 1 
+//        int number1 = 8;
+//        int number2 = 7;
+//        Console.WriteLine(number1 > number2);
+//        //4.2
+//        int a1 = 10;
+//        int a2 = 8;
+//        Console.WriteLine(a1 <= a2);
 
-        int[,,] arr4 = new int[1, 2, 3]
-           {{{45,80,87}, {52,85,98}}};
+//        // 5)ჩაწერეთ შემდეგი პირობა Boolean ლოგიკური ოპერატორის გამოყენებით: A>B  და A>C, A>B ან A>C. როცა A=5,B=4,C=7
+//        int A = 5;
+//        int B = 4;
+//        int C = 7;
+
+//        Console.WriteLine(A > B && A > C);
+//        Console.WriteLine(A > B || A > C);
+
+//        // 6)შექმენით რიცხვითი ტიპის, 10 ელემენტისაგან შემდგარი მასივი. კონსოლზე გამოიტანეთ პირველი და მეშვიდე ელემენტი.
+//        int[] numbers = new int[10];
+//        numbers[0] = 10;
+//        numbers[1] = 11;
+//        numbers[2] = 12;
+//        numbers[3] = 13;
+//        numbers[4] = 14;
+//        numbers[5] = 15;
+//        numbers[6] = 16;
+//        numbers[7] = 17;
+//        numbers[8] = 18;
+//        numbers[9] = 19;
+
+//        Console.WriteLine(numbers[0]);
+//        Console.WriteLine(numbers[6]);
+
+//        // 7)7) შექმენით მასივები შემდეგი განზომილებებით და შეავსე რიცხვებით:
+//        //ორგანზომილებიანი[3, 2], [4,2]
+
+//        int[,] array2 = new int[3, 2]
+//       { { 5, 6 },
+//         { 7, 8 },
+//         { 9, 10 } };
+
+//        int[,] array4 = new int[4, 2]
+//       { { 5, 6 },
+//         { 7, 8 },
+//         { 9, 10 },
+//         {11,12 } };
+
+//        //სამგანზომილებიანი[2, 2, 2], [1,2,3]
+
+//        int[,,] arr3 = new int[2, 2, 2]
+//       { 
+//         {{11,24 },{18,48}},
+//         {{ 62,72}, { 91,10}}
+//       };
 
 
+
+//        int[,,] arr4 = new int[1, 2, 3]
+//           {{{45,80,87}, {52,85,98}}};
 
 
 
@@ -103,8 +101,10 @@ class MyMain
 
 
 
-    }
-}
+
+
+//    }
+//}
 
 
 
--- Tera Bank project/Sophie.cs
+++ Tera Bank project/Sophie.cs
@@ -1,22 +1,125 @@
 ﻿
 
 
+using System.Runtime.CompilerServices;
+
 class Program
 {
-    static void Main(string[] args)
-    {
+    //static void Main(string[] args)
+    //{
 
-        MyFunction my = Sum;
-        int result = my(5, 4);
-        Console.WriteLine(result);
+        //1) შექმენით მეთოდი, რომელსაც ექნება სამი გადასაცემი პარამეტრი: სახელი, მიმდინარე თარიღი და ასაკი.
+        //სამივე მონაცემი უნდა შევიყვანოთ კონსოლიდან და მეთოდის საშუალებით უნდა დაბრუნდეს მნიშვნელობა; შედეგი კი დაიბეჭდოს კონსოლზე
 
-    }
-    delegate int MyFunction(int a, int b);
-    public static int Sum(int a, int b)
-    {
-        return a + b;
-    }
 
+        //        MyFirstMethod("Sophie", "DateTime", 33);
+        //    Console.Write("Current Date is : ");
+        //            DateTime Now = DateTime.Now;
+        //    Console.WriteLine(DateTime.Now.ToString("M/d/yyyy"));
+        //            Console.ReadLine();
+        //        }
+
+        //static void MyFirstMethod(string name, string Date, int age)
+
+
+        //{
+        //    Console.WriteLine(name + ", " + age);
+
+        //}
+
+        // 2) შექმენით მეთოდი:  თუ კონსოლიდან გადავეცი + უნდა დააბრუნოს გადაცემული ორი რიცხვის ჯამი,
+        // თუ კონსოლიდან გადავეცი * უნდა დააბრუნოს ორი რიცხვის ნამრავლი.
+
+        // 2.1
+        //static int MyMethod(int x, int y)
+        //{
+        //    return x + y;
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine(MyMethod(15, 111));
+        //}
+
+        //2.2
+        //static int MyMethod(int x, int y)
+        //{
+        //    return x * y;
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine(MyMethod(40, 10));
+        //}
+
+
+
+        //3)შექმენით მეთოდი, რომელსაც ექნება ერთი პარამეტრი.თუ გადაცემული რიცხვი იქნება უარყოფითი დააბრუნოს 0,
+        //    //წინააღმდეგ შემთხვევაში დააბრუნოს გადაცემული რიცხვი.
+
+        //    static int MyMethod(int x)
+        //{
+
+        //    int result;
+
+        //    if (x < 0)
+        //    {
+        //        result = 0;
+        //    }
+        //    else
+        //    {
+        //        result = x;
+
+        //        return result;
+        //    }
+
+        //    {
+        //        Console.WriteLine(MyMethod(-50));
+        //    }
+        //}
+
+
+
+
+
+        //4) შექმენით მეთოდი, რომელიც გამოითვლის რამდენი კგ ტვირთი იქნება გადასაზიდი.გადასაცემი პარამეტრები იქნება ერთი ერთეულის წონა და რაოდენობა.
+        //წონა იქნება default მნიშვნელობის და მისი მნიშვნელობა იქნება 50.
+
+        static void New()
+        {
+
+        Console.WriteLine("Enter the quantity: ");
+        int quantity = Convert.ToInt32(Console.ReadLine());
+
+        int defaultweight = 50;
+        Console.WriteLine("defaultweight is:" + defaultweight);
+
+        Console.WriteLine("The result of kg is: " + (quantity * defaultweight));
+        }
+
+
+
+
+
+        //5)შექმენით ორი გადატვირთული მეთოდი.თუ გადაეცა ორი პარამეტრი, დააბრუნოს სახელი და გვარი. თუ გადაეცა სამი პარამეტრი,
+        //დააბრუნოს თქვენი სახელი გვარი და დაბადების წელი.
+
+
+        //static string  Bio(string name, string Lastname)
+        //{
+        //    return name + Lastname;
+        //}
+
+        //static string Bio(string name, string Lastname, string birthdate)
+        //{
+        //    return name + Lastname + birthdate;
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    string x1 = Bio("Sophie", "Martiashvili");
+        //    string x2 = Bio("Sophie", "Martiashvili", "06/12/1989");
+        //    Console.WriteLine("name and lastname: " + "  " + x1);
+        //    Console.WriteLine("name, lastname, age : " + "  " + x2);
+    //}
 
 
 
@@ -24,3 +127,5 @@ class Program
 
 
 }
+
+
